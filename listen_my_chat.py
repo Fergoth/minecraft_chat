3050569b8caba43562ac679999590edea8483e54
fergoth <fergothew@gmail.com>
2025-11-14 12:57:31 +0300

    rename

---
+++ listen_my_chat.py
@@ -0,0 +1,37 @@
+import asyncio
+import datetime
+import logging
+
+import aiofiles
+import configargparse
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("based")
+
+
+async def main():
+    reader, _ = await asyncio.open_connection(args.host, args.port)
+    while True:
+        data = await reader.readuntil(separator=b"\n")
+        current_time = datetime.datetime.now()
+        logging.INFO(
+            f"[{current_time.strftime('%d.%m.%y %H:%M')}] {data.decode()}", end=""
+        )
+        async with aiofiles.open("chat_log.txt", mode="a") as f:
+            await f.write(
+                f"[{current_time.strftime('%d.%m.%y %H:%M')}] {data.decode()}"
+            )
+
+
+if __name__ == "__main__":
+    parser = configargparse.ArgParser(
+        auto_env_var_prefix="CHAT_",
+    )
+    parser.add("--host", default="minechat.dvmn.org", help="Hostname")
+    parser.add("--port", default=5000, help="Port")
+    parser.add("--logfile", default="chat_log.txt", help="Log file")
+    args = parser.parse_args()
+    asyncio.run(main())
