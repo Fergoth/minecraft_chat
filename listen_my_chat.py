28e233a3c4297cdd9d6f5fbdc49df5c004263a55
fergoth <fergothew@gmail.com>
2025-11-14 13:57:06 +0300

    logging fix

--- listen_my_chat.py
+++ listen_my_chat.py
@@ -16,7 +16,7 @@ async def main():
     while True:
         data = await reader.readuntil(separator=b"\n")
         current_time = datetime.datetime.now()
-        logging.INFO(
+        logger.debug(
             f"[{current_time.strftime('%d.%m.%y %H:%M')}] {data.decode()}", end=""
         )
         async with aiofiles.open("chat_log.txt", mode="a") as f:
